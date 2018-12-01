d53ca839f4e5a53421dc167c38b55216ed5557b6
naveenr414 <dsfan414@gmail.com>
2018-12-01 11:41:29 -0500

    Got the login page working

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask
+from flask import Flask, session, redirect, url_for
 import loginApp
 
 app = Flask(__name__)
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 @app.route('/')
-def main():
-    return "Main"
+def index():
+    if('username' in session):
+        return session['username']
+    else:
+        return "Main"
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return loginApp.login()
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 app.run(host='127.0.5.1', port= 8081)
--- loginApp.py
+++ loginApp.py
@@ -1,7 +1,42 @@
-from flask import Flask, session
+from flask import Flask, session, redirect, url_for, render_template, request
+import hashlib
+import sqlite3
 
 def login():
-    if('username' in session):
-        return "Logged in"
-    else:
-        return "Not Logged in"
+    
+    """Log in a registered user by adding the user id to the session."""
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        h = hashlib.md5()
+        h.update(password.encode())
+        password = h.hexdigest()
+
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        
+        error = None
+        user = c.execute(
+            'SELECT * FROM users WHERE username = ?', (username,)
+        ).fetchone()
+
+        d = {}
+
+        if user is not None:
+            d = {'name':user[0],'username':user[1],'password':user[2]}
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif d['password'] !=  password:
+            error = 'Incorrect password.'
+        if error is None:
+            # store the user id in a new session and return to the index
+            session.clear()
+            session['username'] = d['username']
+            return redirect(url_for('index'))
+
+        return render_template("login.html",error=error)
+
+    print("HERE")
+
+    return render_template('login.html',error="")
