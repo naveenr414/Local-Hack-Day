ddfd8c5938c3f8d3b70f23015ec5838f3824be71
naveenr414 <dsfan414@gmail.com>
2018-12-01 11:12:53 -0500

    Created database

--- app.py
+++ app.py
@@ -3,15 +3,12 @@ import loginApp
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def helloIndex():
-    return 'Hello World from Python Flask!'
+@app.route('/')
+def main():
+    return "Main"
 
 @app.route('/login')
 def login():
     return loginApp.login()
-
-@app.route('/fakelogin')
-def fakeLogin()
 
 app.run(host='127.0.5.1', port= 8081)
