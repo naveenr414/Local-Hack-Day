1623d29687e4f7cf470991b4544661544886b9b9
naveenr414 <dsfan414@gmail.com>
2018-12-01 18:38:34 -0500

    Changed usernames to names

--- clubApp.py
+++ clubApp.py
@@ -8,17 +8,26 @@ def club(clubname):
     print(clubname,clubInfo)
     if(clubInfo):
         description = clubInfo[1]
-        followers = c.execute("SELECT * FROM following WHERE user2=?",(clubname,))
+        followers = c.execute("SELECT * FROM following WHERE user2=?",(clubname,)).fetchall()
         followers = [i[0] for i in followers]
-        reviews = c.execute("SELECT * from reviews WHERE clubname=?",(clubname,))
+
+        names=[]
+        for follow in followers:
+            names.append(c.execute("SELECT * FROM users WHERE username=?",(follow,)).fetchone()[0])
+        
+        reviews = c.execute("SELECT * from reviews WHERE clubname=?",(clubname,)).fetchall()
+
+        reviews = sorted(reviews,key=lambda x: x[2],reverse=True)
+
         d = []
         for i in reviews:
             l = {}
-            l["reviewer"] = i[0]
+            n = c.execute("SELECT * FROM users WHERE username=?",(i[0],)).fetchone()[0]
+            l["reviewer"] = n
             l["time"] = i[2]
             l["review"] = i[1]
             d.append(l)
-        return render_template("club.html",name=clubname, description=description,followers=followers,reviews=d,username=session["username"])
+        return render_template("club.html",name=clubname, description=description,followers=names,reviews=d,username=session["username"])
     else:
         return redirect(url_for("index"))
 
